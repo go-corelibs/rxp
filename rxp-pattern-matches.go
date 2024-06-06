79cefbb1706fc3999c1f92b53accc8b61baf69d2
Kevin C. Krinke <kevin@krinke.ca>
2024-06-06 13:21:07 -0400

    rxp (pattern matches): add Valid and Len methods to Matches and SubMatches; add Len method to SubMatch

--- rxp-pattern-matches.go
+++ rxp-pattern-matches.go
@@ -17,6 +17,24 @@ package rxp
 // Matches is the list of all SubMatches for each of the Pattern matches
 type Matches []SubMatches
 
+func (m Matches) Valid() bool {
+	for _, sm := range m {
+		if !sm.Valid() {
+			return false
+		}
+	}
+	return true
+}
+
+func (m Matches) Len() int {
+	if lm := len(m); lm > 0 {
+		if lmm := len(m[lm-1]); lmm > 0 {
+			return m[lm-1].End() - m[0].Start()
+		}
+	}
+	return -1
+}
+
 func (m Matches) Start() int {
 	if len(m) > 0 {
 		return m[0].Start()
@@ -33,6 +51,24 @@ func (m Matches) End() int {
 
 // SubMatches is the list of SubMatch indices for a single matched Pattern
 type SubMatches []SubMatch
+
+func (m SubMatches) Valid() bool {
+	for _, sm := range m {
+		if !sm.Valid() {
+			return false
+		}
+	}
+	return true
+}
+
+func (m SubMatches) Len() int {
+	if lm := len(m); lm > 0 {
+		if lmm := len(m[lm-1]); lmm > 0 {
+			return m[lm-1].End() - m[0].Start()
+		}
+	}
+	return -1
+}
 
 func (m SubMatches) Start() int {
 	if len(m) > 0 && len(m[0]) > 0 {
@@ -52,6 +88,10 @@ func (m SubMatches) End() int {
 
 // SubMatch is a single pair of start and end input rune indices
 type SubMatch []int
+
+func (m SubMatch) Valid() bool {
+	return len(m) > 1
+}
 
 func (m SubMatch) Len() int {
 	if len(m) > 1 {
