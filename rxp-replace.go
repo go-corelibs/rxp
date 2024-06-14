5347d5c1a413f937720557dc64010668c9649a7b
Kevin C. Krinke <kevin@krinke.ca>
2024-06-14 13:00:30 -0400

    rxp (replace): renamed Replacement to Replacer; refactored Replace, Replacer and Transform to support generic []rune|[]byte|string types; refactored ToLower and ToUpper for type-specific operations; added better docstrings

--- rxp-replace.go
+++ rxp-replace.go
@@ -15,44 +15,110 @@
 package rxp
 
 import (
+	"bytes"
 	"strings"
+	"unicode"
 )
 
 // Transform is the function signature for non-rxp string transformation
 // pipeline stages
-type Transform func(input string) (output string)
+type Transform[V []rune | []byte | string] func(input V) (output V)
 
-// Replacement is a Segment.String replacement function
-type Replacement func(s Segment) (replaced string)
+// Replacer is a Replace processor function
+//
+// The captured argument is the result of the Pattern match process and is
+// composed of the entire matched text as the first item in the captured list,
+// and any Pattern capture groups following
+//
+// The modified string argument is the output of the previous Replacer in the
+// Replace process, or the original matched input text if this is the first
+// Replacer in the process
+type Replacer[V []rune | []byte | string] func(input *InputReader, captured [][2]int, modified V) (replaced V)
 
-// Replace is a Replacement pipeline
-type Replace []Replacement
+// Replace is a Replacer pipeline
+type Replace[V []rune | []byte | string] []Replacer[V]
 
-// WithReplace replaces matches using a Replacement function
-func (r Replace) WithReplace(replacer Replacement) Replace {
+func (r Replace[V]) process(input *InputReader, captured [][2]int, data V) (replaced V) {
+	replaced = data
+	for _, rpl := range r {
+		replaced = rpl(input, captured, replaced)
+	}
+	return
+}
+
+// WithReplace replaces matches using a Replacer function
+func (r Replace[V]) WithReplace(replacer Replacer[V]) Replace[V] {
 	return append(r, replacer)
 }
 
 // WithTransform replaces matches with a Transform function
-func (r Replace) WithTransform(transform Transform) Replace {
-	return append(r, func(m Segment) (replaced string) {
-		return transform(m.String())
+func (r Replace[V]) WithTransform(transform Transform[V]) Replace[V] {
+	return append(r, func(input *InputReader, captured [][2]int, text V) (replaced V) {
+		return transform(text)
 	})
 }
 
-// WithText replaces matches with the plain text given
-func (r Replace) WithText(text string) Replace {
-	return append(r, func(s Segment) (replaced string) {
-		return text
+// WithLiteral replaces matches with the literal value given
+func (r Replace[V]) WithLiteral(data V) Replace[V] {
+	return append(r, func(input *InputReader, captured [][2]int, text V) (replaced V) {
+		return data
 	})
 }
 
-// ToLower is a convenience wrapper around WithTransform and strings.ToLower
-func (r Replace) ToLower() Replace {
-	return r.WithTransform(strings.ToLower)
+// ToLower is a convenience method which, based on Replace type, will use one of
+// the following methods to lower-case all the lower-case-able values
+//
+//	| Type   | Function        |
+//	|--------|-----------------|
+//	| []rune | unicode.ToLower |
+//	| []byte | bytes.ToLower   |
+//	| string | strings.ToLower |
+func (r Replace[V]) ToLower() Replace[V] {
+	return append(r, func(input *InputReader, captured [][2]int, data V) (replaced V) {
+		switch t := interface{}(&data).(type) {
+		case *[]rune:
+			slice := make([]rune, len(*t))
+			for idx, ch := range *t {
+				slice[idx] = unicode.ToLower(ch)
+			}
+			return interface{}(slice).(V)
+		case *[]byte:
+			slice := bytes.ToLower(*t)
+			return interface{}(slice).(V)
+		case *string:
+			slice := strings.ToLower(*t)
+			return interface{}(slice).(V)
+		default:
+			panic("the universe is broken")
+		}
+	})
 }
 
-// ToUpper is a convenience wrapper around WithTransform and strings.ToUpper
-func (r Replace) ToUpper() Replace {
-	return r.WithTransform(strings.ToUpper)
+// ToUpper is a convenience method which, based on Replace type, will use one of
+// the following methods to upper-case all the upper-case-able values
+//
+//	| Type   | Function        |
+//	|--------|-----------------|
+//	| []rune | unicode.ToUpper |
+//	| []byte | bytes.ToUpper   |
+//	| string | strings.ToUpper |
+func (r Replace[V]) ToUpper() Replace[V] {
+	return append(r, func(input *InputReader, captured [][2]int, data V) (replaced V) {
+		switch t := interface{}(&data).(type) {
+		case *[]rune:
+			slice := make([]rune, len(*t))
+			for idx, ch := range *t {
+				slice[idx] = unicode.ToUpper(ch)
+			}
+			return interface{}(slice).(V)
+		case *[]byte:
+			slice := bytes.ToUpper(*t)
+			return interface{}(slice).(V)
+		case *string:
+			slice := strings.ToUpper(*t)
+			return interface{}(slice).(V)
+		default:
+			panic("the universe is broken")
+		}
+	})
 }
